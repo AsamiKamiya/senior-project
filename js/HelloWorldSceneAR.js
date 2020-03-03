706e869281ea03bc3ea50fc0b1ea2804dd07001a
Mary <mary@codechrysalis.io>
2020-03-03 16:04:40 +0900

    Implementing first tamamon

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 import {
   ViroARScene,
@@ -16,16 +16,18 @@ import {
   ViroARPlaneSelector,
   ViroNode,
   ViroAnimations,
-} from 'react-viro';
+  ViroARTrackingTargets,
+  ViroARImageMarker,
+  ViroARTrackingTarget
+} from "react-viro";
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR..."
     };
 
     // bind 'this' to functions
@@ -34,39 +36,48 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroARPlaneSelector />
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/>
-        <Viro3DObject source={require('./res/cat.obj')}
-              position={[-0.0, -5.5, -1.15]}
-              // materials={["suica"]}
-              resources={[require('./res/cat.mtl'),
-                       require('./res/cat.png')]}
-              type="OBJ" 
-              scale={[.1,.1,.1]}
-              position={[0, -1, -1]}/>
-        <Viro3DObject source={require('./res/cat2.obj')}
-              position={[-0.0, -5.5, -1.15]}
-              // materials={["suica"]}
-              resources={[require('./res/cat2.mtl'),
-                       require('./res/cat2.png')]}
-              type="OBJ" 
-              scale={[.1,.1,.1]}
-              position={[0.5, -1, -1]}/>
-        <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
-          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-        <ViroNode position={[0,-1,0]} dragType="FixedToWorld" onDrag={()=>{}}>
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+
+        {/*
+
         <Viro3DObject
-            source={require('./res/emoji_smile/emoji_smile.vrx')}
-            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                require('./res/emoji_smile/emoji_smile_normal.png'),
-                require('./res/emoji_smile/emoji_smile_specular.png')]}
-            position={[0, .5, 0]}
-            scale={[.2, .2, .2]}
-            type="VRX" />
-        </ViroNode>
+          source={require("./res/cat.obj")}
+          position={[-0.0, -5.5, -1.15]}
+          // materials={["suica"]}
+          resources={[require("./res/cat.mtl"), require("./res/cat.png")]}
+          type="OBJ"
+          scale={[0.1, 0.1, 0.1]}
+          position={[0, -1, -1]}
+        />
+    */}
+
+        <ViroARImageMarker target={"targetOne"}>
+          <Viro3DObject
+            source={require("./res/cat2.obj")}
+            position={[-0.0, -1, -1]}
+            // materials={["suica"]}
+            resources={[require("./res/cat2.mtl"), require("./res/cat2.png")]}
+            type="OBJ"
+            scale={[0.1, 0.1, 0.1]}
+            position={[0.5, -1, -1]}
+          />
+        </ViroARImageMarker>
+
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
       </ViroARScene>
     );
   }
@@ -74,35 +85,32 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "It works!!(^o^)"
+        text: "It works!!(^o^)"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+      this.setState({
+        text: "It's not working"
+      });
     }
   }
 }
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: 'pink',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+    color: "pink",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
 });
-ViroMaterials.createMaterials({
-  grid: {
-    diffuseTexture: require('./res/grid_bg.jpg'),
-  },
-});
-ViroAnimations.registerAnimations({
-  rotate: {
-    properties: {
-      rotateY: "+=90"
-    },
-    duration: 250, //.25 seconds
-  },
+
+ViroARTrackingTargets.createTargets({
+  targetOne: {
+    source: require("./res/FinalMark.jpg"),
+    orientation: "Up",
+    physicalWidth: 0.1
+  }
 });
 
 module.exports = HelloWorldSceneAR;
