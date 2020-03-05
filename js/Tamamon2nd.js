71a418e5797b885be291f549c00249dcea33e752
kamiya <petit0608@gmail.com>
2020-03-05 10:43:20 +0900

    add animation 2nd.

--- js/Tamamon2nd.js
+++ js/Tamamon2nd.js
@@ -14,76 +14,129 @@ import {
   ViroAnimations,
   ViroARTrackingTargets,
   ViroARImageMarker,
+  ViroButton,
+  ViroImage
 } from "react-viro";
 
-export default class HelloWorldSceneAR extends Component {
+let fedCount = 0;
+
+export default class Tamamon2nd extends Component {
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
       text: "Initializing AR...",
-      animeFlg : true,
+      fed: false,
+      animate: false,
+      animeFlg: 0
     };
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this._onTap = this._onTap.bind(this);
   }
 
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
         <ViroText
-          text={this.state.text}
-          scale={[0.3, 0.3, 0.3]}
-          position={[0, 0, -1]}
-          style={styles.helloWorldTextStyle}
+          text="Please scan the AR Marker!"
+          position={[0, 0, -5]}
+          style={styles.announceTextStyle}
         />
-<Viro3DObject
-          source={require("./res/speechBubble.obj")}
-          position={[-0.0, -5.5, -1.15]}
-          // materials={["suica"]}
-          resources={[require("./res/speechBubble.mtl"), require("./res/speechBubble.png")]}
-          type="OBJ"
-          scale={[0.1, 0.1, 0.1]}
-          position={[0.2, -0.8, -1]}
-        />
-        
-      {this.state.animeFlg ? 
-        <Viro3DObject
-          source={require("./res/tamamon2nd/cat.obj")}
-          position={[-0.0, -5.5, -1.15]}
-          // materials={["suica"]}
-          resources={[require("./res/tamamon2nd/cat.mtl"), require("./res/tamamon2nd/cat.png")]}
-          type="OBJ"
-          scale={[0.1, 0.1, 0.1]}
-          position={[0, -1, -1]}
-        />
-      :<Viro3DObject
-      source={require("./res/tamamon2nd/cat-a1.obj")}
-      position={[-0.0, -5.5, -1.15]}
-      // materials={["suica"]}
-      resources={[require("./res/tamamon2nd/cat-a1.mtl"), require("./res/tamamon2nd/cat-a1.png")]}
-      type="OBJ"
-      scale={[0.1, 0.1, 0.1]}
-      position={[0, -1, -1]}
-    />
-      }
-   
-
-
         <ViroARImageMarker
           target={"targetOne"}
           pauseUpdates={this.state.pauseUpdates}
         >
-          <Viro3DObject
-            source={require("./res/tamamon2nd/cat.obj")}
-            //position={[-0.0, -1, -1]}
-            resources={[require("./res/tamamon2nd/cat.mtl"), require("./res/tamamon2nd/cat.png")]}
-            type="OBJ"
+          <ViroText
+            text={this.state.text}
             scale={[0.3, 0.3, 0.3]}
-            animation={{name: "rotate", run: true, loop: true}}
+            position={[0.6, 0.08, -0.9]}
+            style={styles.helloWorldTextStyle}
           />
+          <Viro3DObject
+            source={require("./res/speechBubble.obj")}
+            resources={[
+              require("./res/speechBubble.mtl"),
+              require("./res/speechBubble.png")
+            ]}
+            type="OBJ"
+            scale={[0.2, 0.2, 0.2]}
+            position={[0.2, -0.8, -1]}
+          />
+          {this.state.animeFlg === 1 ? (
+            <ViroImage
+              source={require("./res/heart.png")}
+              position={[0.3, -0.3, -0.8]}
+              scale={[0.2, 0.2, 0.2]}
+              opacity={0}
+              transformBehaviors={["billboard"]}
+              animation={{ name: "heartAnimation", run: this.state.fed }}
+            />
+          ) : (
+            <ViroText
+              text=""
+              scale={[0.3, 0.3, 0.3]}
+              Ï
+              position={[0.6, 0.08, -0.9]}
+              style={styles.helloWorldTextStyle}
+            />
+          )}
+
+          {this.state.animeFlg === 0 || this.state.animeFlg === 2 ? (
+            <Viro3DObject
+              source={require("./res/tamamon2nd/cat.obj")}
+              position={[-0.0, -5.5, -1.15]}
+              // materials={["suica"]}
+              resources={[
+                require("./res/tamamon2nd/cat.mtl"),
+                require("./res/tamamon2nd/cat.png")
+              ]}
+              type="OBJ"
+              scale={[0.2, 0.2, 0.2]}
+              position={[0, -1.3, -1]}
+              // animation={
+              //   this.state.fed
+              //     ? { name: "catBoundNo", run: this.state.fed }
+              //     : { name: "catNormal", run: !this.state.fed, loop: true }
+              // }
+              animation={{ name: "catBoundNo", run: this.state.fed }}
+            />
+          ) : this.state.animeFlg === 1 ? (
+            <Viro3DObject
+              source={require("./res/tamamon2nd/cat-a1.obj")}
+              position={[-0.0, -5.5, -1.15]}
+              resources={[
+                require("./res/tamamon2nd/cat-a1.mtl"),
+                require("./res/tamamon2nd/cat-a1.png")
+              ]}
+              type="OBJ"
+              scale={[0.2, 0.2, 0.2]}
+              position={[0, -1.3, -1]}
+              animation={{ name: "catBound", run: this.state.fed }}
+            />
+          ) : (
+            <Viro3DObject
+              source={require("./res/tamamon2nd/cat-a2.obj")}
+              position={[-0.0, -5.5, -1.15]}
+              // materials={["suica"]}
+              resources={[
+                require("./res/tamamon2nd/cat-a2.mtl"),
+                require("./res/tamamon2nd/cat-a2.png")
+              ]}
+              type="OBJ"
+              scale={[0.2, 0.2, 0.2]}
+              position={[0, -1.3, -1]}
+              animation={{ name: "catBoundAngry", run: this.state.fed }}
+            />
+          )}
+          <ViroButton
+            source={require("./res/foodIcon.png")}
+            position={[-0, -1, -0.8]}
+            scale={[0.5, 0.5, 0.5]}
+            onClick={this._onTap}
+          ></ViroButton>
         </ViroARImageMarker>
 
         <ViroAmbientLight color={"#aaaaaa"} />
@@ -100,18 +153,6 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   _onInitialized(state, reason) {
-    let count = 0;
-    setInterval(()=> {
-      if(count < 5){
-        if(this.state.animeFlg){
-          this.setState({animeFlg: false})
-        }else {
-          this.setState({animeFlg: true})
-        }
-        count++;
-      } 
-    }, 1800);
-
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
         text: "Welcome to Tamamon"
@@ -122,13 +163,45 @@ export default class HelloWorldSceneAR extends Component {
       });
     }
   }
+  _onTap() {
+    ++fedCount;
+
+    if (fedCount === 1) {
+      this.setState({
+        text: "Thank you for feeding me!",
+        fed: true,
+        animeFlg: fedCount
+      });
+    } else if (fedCount === 2) {
+      this.setState({
+        text: "I could totally eat more...",
+        animeFlg: fedCount
+      });
+    } else if (fedCount === 3) {
+      this.setState({
+        text: "I'm really full!!",
+        animeFlg: fedCount
+      });
+    } else if (fedCount >= 5) {
+      this.setState({
+        text: "I can't eat anymore..!"
+      });
+    }
+  }
 }
 
 var styles = StyleSheet.create({
-  helloWorldTextStyle: {
+  announceTextStyle: {
     fontFamily: "Arial",
     fontSize: 30,
-    color: "pink",
+    color: "white",
+    textAlignVertical: "center",
+    textAlign: "center"
+  },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 20,
+    color: "black",
     textAlignVertical: "center",
     textAlign: "center"
   }
@@ -142,9 +215,62 @@ ViroARTrackingTargets.createTargets({
   }
 });
 ViroAnimations.registerAnimations({
+  moveUp: {
+    properties: { positionY: "+=0.3", opacity: 1.0 },
+    duration: 500,
+    easing: "bounce"
+  },
+  boundUp: {
+    properties: { positionY: "+=0.1", opacity: 1.0 },
+    duration: 100,
+    easing: "bounce"
+  },
+  boundDown: {
+    properties: { positionY: "-=0.1", opacity: 1.0 },
+    duration: 50,
+    easing: "bounce"
+  },
+  scaleUp: {
+    properties: { scaleX: 1, scaleY: 1, scaleZ: 1 },
+    duration: 50,
+    easing: "easeineaseout"
+  },
+  scaleDown: {
+    properties: { scaleX: 0.1, scaleY: 0.1, scaleZ: 0.1 },
+    duration: 50,
+    easing: "easeineaseout"
+  },
   rotate: {
-    properties:{scaleX:1, scaleY:.6, scaleZ:1, opacity: 1},
-    easing:"Bounce", duration: 5000},
+    properties: { rotateY: "+=90", opacity: 1.0 },
+    duration: 250
+  },
+  moveLeft: {
+    properties: { positionX: "+=0.05" },
+    duration: 100,
+    easing: "bounce"
+  },
+  moveRight: {
+    properties: { positionX: "-=0.05" },
+    duration: 100,
+    easing: "bounce"
+  },
+  tapAnimation: [["scaleUp", "scaleDown"]],
+  catNormal: [
+    [
+      "moveLeft",
+      "moveLeft",
+      "moveLeft",
+      "moveLeft",
+      "moveRight",
+      "moveRight",
+      "moveRight",
+      "moveRight"
+    ]
+  ],
+  catBoundNo: [["moveLeft", "moveRight", "moveLeft", "moveRight"]],
+  catBound: [["boundUp", "boundDown"]],
+  catBoundAngry: [["catBound", "catBound", "catBound"]],
+  heartAnimation: [["moveUp", "rotate"]]
 });
 
-module.exports = HelloWorldSceneAR;
+module.exports = Tamamon2nd;
