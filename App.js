6498fda49441a03ea11033801adabc4023d96c8f
Mary <mary@codechrysalis.io>
2020-03-20 11:27:43 +0900

    Fixed App.js commenting on deviceID

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import { ViroARSceneNavigator } from "react-viro";
 
 // Device ID module --> to integrate see README
 
-//import DeviceInfo from "react-native-device-info";
+import DeviceInfo from "react-native-device-info";
 import { makeNewUser } from "./graphql/mutations";
 import { userData, devices } from "./graphql/queries";
 import clone from "./utils/clone";
@@ -51,8 +51,8 @@ export default class ViroSample extends Component {
     this.state = {
       navigatorType: defaultNavigatorType,
       sharedProps: sharedProps,
-      //deviceID: DeviceInfo.getUniqueId(),
-      deviceID: "sadness",
+      deviceID: DeviceInfo.getUniqueId(),
+      //deviceID: "sadness",
       wallet: 0,
       serverData: [],
       loaded: false
