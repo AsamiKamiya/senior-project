e1d0210714acb6f8ffeb9a3f10abda82e95c0de3
Mary <mary@codechrysalis.io>
2020-03-20 11:25:54 +0900

    fixed small bug in store and made higher res backgrounds

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import { ViroARSceneNavigator } from "react-viro";
 
 // Device ID module --> to integrate see README
 
-import DeviceInfo from "react-native-device-info";
+//import DeviceInfo from "react-native-device-info";
 import { makeNewUser } from "./graphql/mutations";
 import { userData, devices } from "./graphql/queries";
 import clone from "./utils/clone";
@@ -51,8 +51,8 @@ export default class ViroSample extends Component {
     this.state = {
       navigatorType: defaultNavigatorType,
       sharedProps: sharedProps,
-      deviceID: DeviceInfo.getUniqueId(),
-      // deviceID: "TEST_01",
+      //deviceID: DeviceInfo.getUniqueId(),
+      deviceID: "sadness",
       wallet: 0,
       serverData: [],
       loaded: false
@@ -183,7 +183,7 @@ export default class ViroSample extends Component {
     return (
       <View style={{ display: "flex" }}>
         <ImageBackground
-          source={require("./js/res/images/Sprite-0001.gif")}
+          source={require("./js/res/images/Sprite-0001_2.gif")}
           style={{ width: "100%", height: "100%" }}
         >
           <View style={localStyles.inner}>
